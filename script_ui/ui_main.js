84b729d0eb973f71d1bf5092dda87c229a3f7468
Joaquin Urbano Serra <kimbalear@gmail.com>
2022-01-14 22:10:36 +0100

    init2
    work in progress

--- script_ui/ui_main.js
+++ script_ui/ui_main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-    var init_lyt = "lyt_one";
+    var init_lyt = "lyt_init1";
+    var sub_status = "";
 
     switch (init_lyt) {
         case "h_screen":
@@ -15,6 +16,17 @@ $(document).ready(function () {
             break;
 
         case "lyt_init1":
+            $('.h_screen').hide();
+            $(".form_d_entry").hide();
+            $(".int1").show();
+            $(".int1").css({ 'display': 'grid' });
+            break;
+        case "lyt_init2":
+            $('.h_screen').hide();
+            $(".form_d_entry").hide();
+            $(".int1").hide();
+            $(".int2").show();
+            $(".int2").css({ 'display': 'grid' });
             break;
     }
 
@@ -25,7 +37,6 @@ $(document).ready(function () {
         $(this).addClass('ui_active_item');
         $('.h_screen').show();
         $('.lyt_one').hide();
-        $('.lyt_init1').hide();
     });
 
     $("#ly1").click(function () {
@@ -33,12 +44,38 @@ $(document).ready(function () {
         $(this).addClass('ui_active_item');
         $('.h_screen').hide();
         $('.lyt_one').show();
-        $('.lyt_init1').hide();
     });
 
     $("#lyt_init1").click(function () {
         $(this).siblings().removeClass('ui_active_item');
         $(this).addClass('ui_active_item');
     });
+
+    //LYT Data entry
+    $("#individual").click(function () {
+        $(".form_d_entry").hide();
+        $(".int1").show();
+        $(".int1").css({ 'display': 'grid' });
+    });
+    $("#group").click(function () {
+        $(".form_d_entry").hide();
+        $(".int1").show();
+        $(".int1").css({ 'display': 'grid' });
+    });
+    //[END] LYT Data entry
+
+    //LYT INT1 Welcome Video
+    $("#int1_back").click(function () {
+        $(".int1").hide();
+        //$("#video").get(0).stopVideo();
+        $(".form_d_entry").show();
+    });
+
+    $("#int1_next").click(function () {
+        $(".int1").hide();
+        $(".int2").show();
+        $(".int2").css({ 'display': 'grid' });
+    });
+    //[END] LYT INT1 Welcome Video
 
 });
